27d766de4a6048eb32140e72fc670de4fe0289ed
Ahmed Musawir <ahmed.musawir@gmail.com>
2026-06-12 11:11:59 +0600

    12june2026 - dealer locator page made dynamic

--- src/app/(public)/dealer-locator/DealerLocatorContent.tsx
+++ src/app/(public)/dealer-locator/DealerLocatorContent.tsx
@@ -4,14 +4,12 @@ import Head from "next/head";
 import React from "react";
 import Image from "next/image";
 import { getImageUrl } from "@/lib/utils";
-import { dealers } from "./data";
-import {
-  MdOutlinePhone,
-  MdOutlineLocationOn,
-  MdOutlineLanguage,
-} from "react-icons/md";
+import { getDealers } from "@/services/dealerServices";
+import DealerList from "./DealerList";
 
-const DealerLocatorContent = () => {
+const DealerLocatorContent = async () => {
+  const dealers = await getDealers();
+
   return (
     <>
       <Head>
@@ -49,39 +47,7 @@ const DealerLocatorContent = () => {
             will help you locate the perfect DockBloxx dealer.
           </p>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
-            {dealers.map((dealer, index) => (
-              <div
-                key={index}
-                className="bg-gray-100 p-6 rounded-lg shadow-sm text-left flex flex-col gap-4"
-              >
-                <h3 className="text-xl font-semibold text-gray-900">
-                  {dealer.name}
-                </h3>
-
-                <div className="flex items-start gap-3 text-gray-700">
-                  <MdOutlineLocationOn className="w-10 h-10 text-gray-400" />
-                  <p className="mt-2">{dealer.address}</p>
-                </div>
-
-                <div className="flex items-start gap-3 text-gray-700">
-                  <MdOutlinePhone className="w-10 h-10 text-gray-400" />
-                  <p className="mt-2">{dealer.phone}</p>
-                </div>
-
-                <div className="flex items-start gap-3 text-gray-700">
-                  <MdOutlineLanguage className="w-10 h-10 text-gray-400" />
-                  <a
-                    href={dealer.website}
-                    target="_blank"
-                    className="mt-2 border-b-2 border-blue-500"
-                  >
-                    Click to go to site
-                  </a>
-                </div>
-              </div>
-            ))}
-          </div>
+          <DealerList dealers={dealers} />
         </Row>
       </Page>
 
